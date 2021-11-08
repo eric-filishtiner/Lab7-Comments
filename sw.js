3062d5452503c32d3bc8e81db271ed889811aedd
Eric Filishtiner <eric.filishtiner@gmail.com>
2021-11-07 22:31:59 -0800

    tmp

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ const CACHE_NAME = 'lab-7-starter';
 var urlsToCache = [
   'assets/styles/main.css',
   'assets/scripts/main.js',
+  //all linked. IDK why I thought these were needed
   //'assets/scripts/router.js',
   //'assets/components/RecipeCard.js',
   //'assets/components/RecipeExpand.js'
